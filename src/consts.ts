f426c92ec402a846ba498d6549eaa484004f11d0
THETRIPOL89 <48728945+THETRIPOL89@users.noreply.github.com>
2023-04-12 18:21:10 +0200

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Daily Learning.';
+export const SITE_DESCRIPTION = 'Daily blog to save what I learn.';
